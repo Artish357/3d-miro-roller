f3c7443bf0e01cc7955fdb70938b7098d3ac5c50
Artish357 <Artish357@users.noreply.github.com>
2024-08-09 15:58:03 +0000

    deploy: 95f0b27c7adbe23e3eadeb5439549b50df84f874

--- src/types/@3d-dice__dice-box.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "@3d-dice/dice-box" {
-  export default class DiceBox {
-    constructor(options: unknown);
-    roll(input: string): void;
-    init(): Promise<DiceBox>;
-    public onRollComplete: (result: { value: number; rolls: { value: number }[]; mods: object[]; modifier: number}[]) => void;
-  }
-}
--- src/types/@3d-dice__dice-ui.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "@3d-dice/dice-ui" {
-  export class AdvancedRoller {
-    constructor(options: { target: string; onSubmit: (input: string) => void });
-  }
-}
